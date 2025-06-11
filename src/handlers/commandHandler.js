cb57d4bf38c7e482525973884624ce081f464abd
Carlos Henrique <hencarlosdv@gmail.com>
2025-06-11 18:12:32 -0300

    feat: create handle command with initial command

---
+++ src/handlers/commandHandler.js
@@ -0,0 +1,21 @@
+const messageService = require('../services/messageService');
+const client = require('../config/client');
+
+class CommandHandler {
+  constructor() {
+    this.client = client;
+    this.messageService = messageService;
+    this.todosKeywords = ['!todos', '!all', '!everyone'];
+  }
+
+  initialize() {
+    this.client.on('message', async (message) => {
+      if(!message || !this.todosKeywords.includes(message.body.toLowerCase())) return;
+      
+      console.log(`Comando ${message.body} recebido`);
+      await this.messageService.handleTodosCommand(message);
+    });
+  }
+}
+
+module.exports = new CommandHandler();
